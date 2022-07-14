da96d59a96057bd6ded63e569828031df501a1df
Simone <cirino.simone99@gmail.com>
2022-07-14 15:54:42 +0200

    aggiunta una stampa dei dati presi dal db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,13 @@
 @section('pageTitle', 'Home')
 
 @section('mainContent')
-<main>
-    
+<main> 
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                {{$movie->id}} -- {{$movie->title}} -- {{$movie->original_title}}  -- {{$movie->nationality}} -- {{$movie->date}} -- {{$movie->vote}}
+            </li>
+        @endforeach
+    </ul>
 </main>
 @endsection
